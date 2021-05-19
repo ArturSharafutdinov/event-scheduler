ab6fba2e1e5239042b4da865378cf1444533edf9
Artur Sharafutdinov <artur.sharafutdinov101@yandex.ru>
2021-05-19 12:35:12 +0300

    make EventMapper a component

--- src/main/java/ru/ivanov/evgeny/eventscheduler/services/mappers/EventMapper.java
+++ src/main/java/ru/ivanov/evgeny/eventscheduler/services/mappers/EventMapper.java
@@ -1,28 +1,41 @@
 package ru.ivanov.evgeny.eventscheduler.services.mappers;
 
 
-import liquibase.pro.packaged.D;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.yaml.snakeyaml.util.ArrayUtils;
+import ru.ivanov.evgeny.eventscheduler.persistence.dao.AccountRepository;
+import ru.ivanov.evgeny.eventscheduler.persistence.domain.Account;
 import ru.ivanov.evgeny.eventscheduler.persistence.domain.Event;
 import ru.ivanov.evgeny.eventscheduler.persistence.dto.EventDto;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
+@Component
 public class EventMapper {
 
-    public static Event mapToEntity(EventDto eventDto) {
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public Event mapToEntity(EventDto eventDto) {
         Event event = new Event();
         event.setName(eventDto.getName());
         event.setDescription(eventDto.getDescription());
         event.setMaxNumberOfParticipants(eventDto.getMaxNumberOfParticipants());
         event.setPrivate(eventDto.getPrivate());
+
+        //account
+        Account account = accountRepository.findById(eventDto.getOwnerId()).orElseThrow(()->new IllegalArgumentException("No such account"));
+        event.setOwner(account);
+
+        //time
         event.setStartTime(eventDto.getStartTime());
         event.setFinishTime(eventDto.getFinishTime());
+        event.setCreatedTime(LocalDateTime.now());
 
         String coordinates = Arrays.stream(eventDto.getCoordinates())
-                .map(c -> c.toString())
+                .map(Object::toString)
                 .collect(Collectors.joining(" "));
 
         event.setCoordinates(coordinates);
@@ -30,15 +43,21 @@ public class EventMapper {
         return event;
     }
 
-    public static EventDto mapToDto(Event event) {
+    public EventDto mapToDto(Event event) {
         EventDto eventDto = new EventDto();
         eventDto.setName(event.getName());
         eventDto.setDescription(event.getDescription());
         eventDto.setMaxNumberOfParticipants(event.getMaxNumberOfParticipants());
         eventDto.setPrivate(event.getPrivate());
+        eventDto.setOwnerId((Long) event.getId());
+
+        //time
+        eventDto.setCreatedTime(event.getCreatedTime());
+        eventDto.setCompletedTime(event.getCompletedTime());
         eventDto.setStartTime(event.getStartTime());
         eventDto.setFinishTime(event.getFinishTime());
 
+        //coordinates
         Double[] coordinates = Arrays.stream(event.getCoordinates().split(" "))
                 .mapToDouble(Double::parseDouble)
                 .boxed()
