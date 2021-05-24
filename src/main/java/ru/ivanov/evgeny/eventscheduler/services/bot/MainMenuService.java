e2950f09ab318def2a8999c992c033df9fa98962
Artur Sharafutdinov <artur.sharafutdinov101@yandex.ru>
2021-05-24 10:54:52 +0300

    add message services

---
+++ src/main/java/ru/ivanov/evgeny/eventscheduler/services/bot/MainMenuService.java
@@ -0,0 +1,54 @@
+package ru.ivanov.evgeny.eventscheduler.services.bot;
+
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Управляет отображением главного меню в чате.
+ *
+ * @author Sergei Viacheslaev
+ */
+@Service
+public class MainMenuService {
+
+    public SendMessage getMainMenuMessage(final long chatId, final String textMessage) {
+        final ReplyKeyboardMarkup replyKeyboardMarkup = getMainMenuKeyboard();
+
+        return createMessageWithKeyboard(chatId, textMessage, replyKeyboardMarkup);
+    }
+
+    private ReplyKeyboardMarkup getMainMenuKeyboard() {
+
+        final ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        List<KeyboardRow> keyboard = new ArrayList<>();
+
+        KeyboardRow row = new KeyboardRow();
+        row.add(new KeyboardButton("Помощь"));
+        keyboard.add(row);
+        replyKeyboardMarkup.setKeyboard(keyboard);
+        return replyKeyboardMarkup;
+    }
+
+    private SendMessage createMessageWithKeyboard(final Long chatId,
+                                                  String textMessage,
+                                                  final ReplyKeyboardMarkup replyKeyboardMarkup) {
+        final SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(chatId.toString());
+        sendMessage.setText(textMessage);
+        if (replyKeyboardMarkup != null) {
+            sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        }
+        return sendMessage;
+    }
+}
