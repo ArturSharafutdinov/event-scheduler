faddc963ab3e5ba13d2aac104c067408e13776b8
Artur Sharafutdinov <artur.sharafutdinov101@yandex.ru>
2021-05-24 10:50:54 +0300

    add bot states

---
+++ src/main/java/ru/ivanov/evgeny/eventscheduler/botapi/BotState.java
@@ -0,0 +1,13 @@
+package ru.ivanov.evgeny.eventscheduler.botapi;
+
+public enum BotState {
+    ASK_NAME,
+    ASK_AGE,
+    ASK_GENDER,
+    ASK_CITY,
+    FILLING_PROFILE,
+    PROFILE_FILLED,
+    SHOW_USER_PROFILE,
+    SHOW_MAIN_MENU,
+    SHOW_HELP_MENU
+}
