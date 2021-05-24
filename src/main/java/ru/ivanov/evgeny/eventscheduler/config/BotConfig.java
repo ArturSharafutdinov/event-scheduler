760fa1daea9ada0277d9509dd98de5363c478df2
Artur Sharafutdinov <artur.sharafutdinov101@yandex.ru>
2021-05-24 10:53:32 +0300

    add messageSource bean and rename bot create method

--- src/main/java/ru/ivanov/evgeny/eventscheduler/config/BotConfig.java
+++ src/main/java/ru/ivanov/evgeny/eventscheduler/config/BotConfig.java
@@ -1,9 +1,11 @@
 package ru.ivanov.evgeny.eventscheduler.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import ru.ivanov.evgeny.eventscheduler.botapi.EventSchedulerBot;
 
 @Configuration
@@ -44,12 +46,22 @@ public class BotConfig {
     }
 
     @Bean
-    public EventSchedulerBot MyEventSchedulerBot() {
+    public EventSchedulerBot eventSchedulerBot() {
         EventSchedulerBot eventSchedulerBot = new EventSchedulerBot();
         eventSchedulerBot.setBotToken(getBotToken());
         eventSchedulerBot.setBotUsername(getBotUsername());
         eventSchedulerBot.setWebHookPath(getWebHookPath());
         return new EventSchedulerBot();
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource
+                = new ReloadableResourceBundleMessageSource();
+
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
     }
 
 }
